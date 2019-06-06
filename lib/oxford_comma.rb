f4b771d43fe18c862d4ceeccc259bcd150e72b30
Paul Holmes <paulholmes1993@gmail.com>
2019-06-06 21:59:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,11 +4,9 @@ def oxford_comma(array)
   
   elsif array.length == 2 
     array.join(' and ')
-    elsif array.length >= 3 
+   else array.length >= 3 
       array[-1] = "and #{array[-1]}"
       array.join(', ')
-    else 
-      nil 
     end 
 end   
   
